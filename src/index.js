c8d7afc7635689d1483f5e32d73bfc6fa02553e6
leigh <l2silver@ryerson.ca>
2016-01-24 17:19:20 -0500

    package and index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { App } from './App';
+import thunk from 'redux-thunk';
 import {
 	createStore
 	, applyMiddleware} from 'redux';
 import reducers from 'reducers.js';
 
 const applyMiddleware(
-	
+	thunk
 	)(createStore)
 
 const store = createStore(reducers);
