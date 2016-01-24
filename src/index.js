9c687198cdbdc6644c8cfeda81515bacd5df1932
leigh <l2silver@ryerson.ca>
2016-01-24 16:31:40 -0500

    added empty reducers file to create store with

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import { App } from './App';
 import {
 	createStore
 	} from 'redux';
+import reducers from 'reducers.js';
+const store = createStore(reducers);
 
 render(<App />, document.getElementById('root'));
