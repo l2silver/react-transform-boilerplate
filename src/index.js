33bb00d5f71e5dc0848d6a89ea8db8830eb96dde
leigh <l2silver@ryerson.ca>
2016-01-24 16:23:13 -0500

    store added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { App } from './App';
+import {
+	createStore
+	} from 'redux';
 
 render(<App />, document.getElementById('root'));
