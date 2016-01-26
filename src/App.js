fa3772533ad94a9ffa0b168446f3045c4e7683eb
Simplistics <leigh@simplistics.ca>
2016-01-26 12:30:18 -0500

    changed router to include signup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { Router, Route, Link, browserHistory } from 'react-router';
-import Example from './example/Example';
+import SignupPage from './user/signup/SignupPage';
 
 export class App extends Component {
   render() {
     return (
        <Router history={browserHistory}>
-        <Route path="/" component={Example}>
-        <Route path="*" component={NoMatch}/>
+        <Route path="/" component={Signup}>
+        <Route path="*" component={Signup}/>
         </Route>
       </Router>
     );
