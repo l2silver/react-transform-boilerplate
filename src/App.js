e428f4705728aa1b0c4527e66ba32a174e50b890
Simplistics <leigh@simplistics.ca>
2016-01-26 12:27:01 -0500

    add react-router and change app file to include router

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
 import React, { Component } from 'react';
-import { NICE, SUPER_NICE } from './colors';
-
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { counter: 0 };
-    this.interval = setInterval(() => this.tick(), 1000);
-  }
-
-  tick() {
-    this.setState({
-      counter: this.state.counter + this.props.increment
-    });
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  render() {
-    return (
-      <h1 style={{ color: this.props.color }}>
-        Counter ({this.props.increment}): {this.state.counter}
-      </h1>
-    );
-  }
-}
+import { Router, Route, Link, browserHistory } from 'react-router';
+import Example from './example/Example';
 
 export class App extends Component {
   render() {
     return (
-      <div>
-        <Counter increment={1} color={NICE} />
-        <Counter increment={5} color={SUPER_NICE} />
-      </div>
+       <Router history={browserHistory}>
+        <Route path="/" component={Example}>
+        <Route path="*" component={NoMatch}/>
+        </Route>
+      </Router>
     );
   }
 }
