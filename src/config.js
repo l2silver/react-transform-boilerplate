fd1412c4e4bf83103f64e1fa1ad85de75e84940d
Simplistics <leigh@simplistics.ca>
2016-01-26 11:03:50 -0500

    add config file'

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export default {
+	uri: {
+		test: 'http://localhost:8888',
+		development: 'http://localhost:8888',
+		stage: 'http://localhost:8888',
+		production: 'http://localhost:8888',
+	}
+}
