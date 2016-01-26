e55ce3328fa79800e790872dd9c53e8afaa6ea30
Simplistics <leigh@simplistics.ca>
2016-01-26 13:44:09 -0500

    first commits

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { Router, Route, Link, browserHistory } from 'react-router';
 import SignupPage from './user/signup/SignupPage';
+import AssemblyIndexPage from './assembly/AssemblyIndexPage';
 
 export class App extends Component {
   render() {
     return (
        <Router history={browserHistory}>
-        <Route path="/" component={SignupPage}>
-        <Route path="*" component={SignupPage}/>
+        <Route path='/' component={SignupPage}>
+        <Route path='/assembly' component={AssemblyIndexPage} />
+        <Route path='*' component={SignupPage}/>
         </Route>
       </Router>
     );
