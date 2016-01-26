b431d09b13e41acfdc4d2ea7218a224dc2a35004
Simplistics <leigh@simplistics.ca>
2016-01-26 13:17:59 -0500

    add to webdev and package

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -22,6 +22,10 @@ module.exports = {
       test: /\.jsx?/,
       loaders: ['babel'],
       include: path.join(__dirname, 'src')
-    }]
+    },{
+      test: /\.scss/,
+      loaders: ['style', 'css', 'sass']
+    }
+    ]
   }
 };
