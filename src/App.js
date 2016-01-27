e609e402025a6789b85e785a40250100ddf19a19
Simplistics <leigh@simplistics.ca>
2016-01-27 10:37:07 -0500

    moved back to regular route setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import { Router, Route, Link, browserHistory } from 'react-router';
 import SignupPage from './user/signup/SignupPage';
-import AssemblyRoutes from './assembly/AssemblyRoutes';
+import AssemblyPages from './assembly/AssemblyPages';
 
 export class App extends Component {
   render() {
     return (
        <Router history={browserHistory}>
-        <Route path='/' component={SignupPage}>
-        <AssemblyRoutes />
-        <Route path='*' component={SignupPage}/>
+
+        <Route path='/' component={Layout}>
+          <Route path='signup' component={SignupPage} />
+          <Route path='assemblies/' component={AssemblyPages}>
+          </Route>
         </Route>
       </Router>
+    );
+  }
+}
+
+export class Layout extends Component {
+  render() {
+    return (
+       <div>
+          {this.props.children}
+       </div>
     );
   }
 }
