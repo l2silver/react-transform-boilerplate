08de1f904fcde015f58fa630b302c9b0ab6e5244
Simplistics <leigh@simplistics.ca>
2016-01-27 11:50:58 -0500

    Add assembly columns page to

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Router, Route, Link, browserHistory } from 'react-router';
 import SignupPage from './user/signup/SignupPage';
 import AssemblyPages from './assembly/AssemblyPages';
+import AssemblyColumnsPages from './assembly/AssemblyColumnsPages/AssemblyColumnsPages';
 
 export class App extends Component {
   render() {
@@ -11,6 +12,8 @@ export class App extends Component {
         <Route path='/' component={Layout}>
           <Route path='signup' component={SignupPage} />
           <Route path='assemblies/' component={AssemblyPages}>
+            <Route path='columns/' component={AssembyColumnsPages}>
+            </Route>
           </Route>
         </Route>
       </Router>
