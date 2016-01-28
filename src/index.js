81574b2b18ffff217fd0c0716a639612ac20d922
leigh <l2silver@ryerson.ca>
2016-01-27 22:42:36 -0500

    properly configured

--- src/index.js
+++ src/index.js
@@ -7,12 +7,20 @@ import {
 	} from 'redux';
 import reducers from './reducers.js';
 import {Provider} from 'react-redux';
+import { syncHistory, routeReducer } from 'react-router-redux'
+import {browserHistory} from 'react-router';
+const reducer = combineReducers(Object.assign({}, reducers, {
+  routing: routeReducer
+}))
+const reduxRouterMiddleware = syncHistory(browserHistory)
 
 const createStoreWithMiddleware = applyMiddleware(
-	
+	reduxRouterMiddleware
 	)(createStore)
 
-const store = createStoreWithMiddleware(reducers);
+const store = createStoreWithMiddleware(reducer);
+
+reduxRouterMiddleware.listenForReplays(store)
 
 render(	<Provider store={store}>
 			<App />
